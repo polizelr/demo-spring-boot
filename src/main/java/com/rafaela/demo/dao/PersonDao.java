8d847802a209c79fa831e06878b4ad035460f36a
Rafaela Polizel <polizelc.rafaela@gmail.com>
2021-01-01 15:37:18 -0300

    Adding an interface for the DAO

---
+++ src/main/java/com/rafaela/demo/dao/PersonDao.java
@@ -0,0 +1,14 @@
+package com.rafaela.demo.dao;
+
+import com.rafaela.demo.model.Person;
+
+import java.util.UUID;
+
+public interface PersonDao {
+    int insertPerson(UUID id, Person person);
+
+    default int insertPerson(Person person) {
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+}
