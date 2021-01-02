6f410972b5903553e157dbc0b2bda45e35e4f4d3
Rafaela Polizel <polizelc.rafaela@gmail.com>
2021-01-02 00:29:32 -0300

    Adding get, put, post and delete http methods

--- src/main/java/com/rafaela/demo/api/PersonController.java
+++ src/main/java/com/rafaela/demo/api/PersonController.java
@@ -3,10 +3,10 @@ package com.rafaela.demo.api;
 import com.rafaela.demo.model.Person;
 import com.rafaela.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RequestMapping("api/v1/person")
 @RestController
@@ -22,4 +22,26 @@ public class PersonController {
     public void addPerson(@RequestBody Person person) {
         personService.addPerson(person);
     }
+
+    @GetMapping
+    public List<Person> getAllPeople(){
+        return personService.getAllPeople();
+    }
+
+    @GetMapping(path="{id}")
+    public Person getPersonById(@PathVariable("id") UUID id){
+        return personService.getPersonById(id)
+                .orElse(null);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void deletePersonById(@PathVariable("id") UUID id){
+        personService.deletePerson(id);
+    }
+
+    @PutMapping(path = "{id}")
+    public void updatePerson(@PathVariable("id") UUID id, @RequestBody Person personToUpdate){
+        personService.updatePerson(id, personToUpdate);
+    }
+
 }
