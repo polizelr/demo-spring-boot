2cb8e1959eb08bf2ebbb4a695dc0831da426a141
Rafaela Polizel <polizelc.rafaela@gmail.com>
2021-01-01 23:12:59 -0300

    Adding controller and spring functionalities in order to create the api

--- src/main/java/com/rafaela/demo/service/PersonService.java
+++ src/main/java/com/rafaela/demo/service/PersonService.java
@@ -2,11 +2,16 @@ package com.rafaela.demo.service;
 
 import com.rafaela.demo.dao.PersonDao;
 import com.rafaela.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PersonService {
     private final PersonDao personDao;
 
-    public PersonService(PersonDao personDao) {
+    @Autowired
+    public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
         this.personDao = personDao;
     }
 
