0cba21a19a5c71d158cdf69727808ac902e37bf1
Rafaela Polizel <polizelc.rafaela@gmail.com>
2021-01-03 16:29:06 -0300

    Connecting to Postgres database

--- src/main/java/com/rafaela/demo/dao/PersonDataAccessService.java
+++ src/main/java/com/rafaela/demo/dao/PersonDataAccessService.java
@@ -1,6 +1,8 @@
 package com.rafaela.demo.dao;
 
 import com.rafaela.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +11,14 @@ import java.util.UUID;
 
 @Repository("postgres")
 public class PersonDataAccessService implements PersonDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public int insertPerson(UUID id, Person person) {
         return 0;
@@ -16,12 +26,24 @@ public class PersonDataAccessService implements PersonDao {
 
     @Override
     public List<Person> selectAllPeople() {
-        return List.of(new Person(UUID.randomUUID(),"From postgres db"));
+        final String sql = "SELECT id, name FROM person";
+        return jdbcTemplate.query(sql, (resultSet, i) -> {
+            return new Person(
+                    UUID.fromString(resultSet.getString("id")),
+                    resultSet.getString("name"));
+        });
     }
 
     @Override
     public Optional<Person> selectPersonById(UUID id) {
-        return Optional.empty();
+        final String sql = "SELECT id, name FROM person WHERE id = ?";
+        Person person = jdbcTemplate.queryForObject(sql, new Object[]{id}, (resultSet, i) -> {
+            return new Person(
+                    UUID.fromString(resultSet.getString("id")),
+                    resultSet.getString("name"));
+        });
+
+        return Optional.ofNullable(person);
     }
 
     @Override
